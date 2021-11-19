f479638f3e515a5321509099c10df7519446c96b
hernandw <hernandw@gmail.com>
2021-11-19 10:52:49 -0300

    configurar routes

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 import express from 'express';
+/* const express = require('express'); */
 const app = express();
 const port = process.env.PORT || 3000;
+/* const router = require('./routes'); */
+import router from './routes/index.js';
 
 app.listen(port, (req, res)=>  {
     console.log(`Servidor Activo on ${port}` )
 });
 
-app.get('/', (req, res)=>{
-    res.send('Servidor Activo')
-});
+// Enlace a routes
+
+app.use('/', router);
+
