efbc9bfb831ab10f09c224e995f30b194d6fb8e6
hernandw <hernandw@gmail.com>
2021-11-22 16:26:42 -0300

    conexion con BBDD

--- backend/controllers/proyectoController.js
+++ backend/controllers/proyectoController.js
@@ -1,34 +1,36 @@
+const data = require("../models/Proyectos");
+
 exports.home = (req, res) => {
-    res.render('index', {
-        nombrePagina: 'Proyectos'
-    })
-}
+  res.render("index", {
+    nombrePagina: "Proyectos",
+  });
+};
 
-exports.formularioProyecto = (req, res)=>{
-    res.render('proyecto', {
-        nombrePagina: 'Nuevos Proyectos'
+exports.formularioProyecto = (req, res) => {
+  res.render("proyecto", {
+    nombrePagina: "Nuevos Proyectos",
+  });
+};
+
+exports.newProyect = async (req, res) => {
+  const { nombre } = req.body;
+  let errores = [];
+
+  if (!nombre) {
+    errores.push({ texto: "Agrega un nombre al proyecto" });
+  }
+
+  if (errores.length > 0) {
+    res.render("proyecto", {
+      nombrePagina: "Nuevo Proyecto",
+      errores,
     });
+  } else {
+    const proyecto = await data.create({ nombre });
+    res.redirect('/');
+  }
+};
 
-}
-
-exports.newProyect = (req, res)=>{
-    const { nombre } = req.body;
-let errores = [];
-
-if(!nombre){
-    errores.push({'texto' : 'Agrega un nombre al proyecto'})
-}
-
-if(errores.length > 0){
-    res.render('proyecto', {
-        nombrePagina: 'Nuevo Proyecto',
-        errores
-    })
-}
-}
-
-
-exports.nosotros = (req, res)=>{
-    res.send('Aqui la página de Nosotros');
-}
-
+exports.nosotros = (req, res) => {
+  res.send("Aqui la página de Nosotros");
+};
